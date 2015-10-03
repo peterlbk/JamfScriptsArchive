5d67fe42e7475affda6b133d97b97db46464b328
peterlbk <peter@loobuyck.net>
2015-10-03 14:44:15 +0200

    Create RemoveJavaFromDave.sh

---
+++ RemoveJavaFromDave.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp Disabled"
+
+mv "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp/jdbcdam.jar" "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp Disabled/jdbcdam.jar"
+mv "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp/javacore.u_xcomp" "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp Disabled/javacore.u_xcomp"
+mv "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp/javaobjs.u_xcomp" "/Applications/Omnis Studio 5.1 Runtime.app/Contents/MacOS/xcomp Disabled/javaobjs.u_xcomp"
